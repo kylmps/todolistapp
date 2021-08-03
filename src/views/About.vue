825047d40fd3a7b870225d6f6f6ff38706834fb3
kylmps <kyleandrei.mapue.iics@ust.edu.ph>
2021-08-03 12:25:15 +0800

    Initial commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About this Todo</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Error sint,
+      necessitatibus, commodi eligendi est alias eaque quas corrupti totam
+      provident delectus laboriosam assumenda molestiae ducimus unde ullam!
+      Fugiat, debitis quia.
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod commodi
+      doloremque molestiae fugiat aliquam! Amet voluptatem incidunt recusandae
+      vitae deleniti et voluptatibus hic. Delectus nam, voluptatibus minima
+      illum iure mollitia.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi blanditiis
+      maiores dolorem ut tempora perspiciatis deleniti porro facere rerum quae
+      reprehenderit quidem ea, perferendis illum fuga eos quas neque id?
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis officia
+      provident, cum in quis modi porro ea repudiandae officiis velit et,
+      placeat nisi ullam optio animi enim cupiditate, sunt ipsa!
+    </p>
   </div>
 </template>
---
+++ src/views/Todo.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="home">
+    <v-text-field
+      v-model="newTaskTitle"
+      @click:append="addTask"
+      @keyup.enter="addTask"
+      class="pa-3"
+      outlined
+      label="Add a task"
+      append-icon="mdi-plus-circle"
+      hide-details
+      clearable
+    ></v-text-field>
+    <v-list class="pt-0" flat>
+      <div v-for="task in tasks" :key="task.id">
+        <v-list-item
+          @click="doneTask(task.id)"
+          :class="{ 'blue lighten-5': task.done }"
+        >
+          <template v-slot:default>
+            <v-list-item-action>
+              <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title
+                :class="{ 'text-decoration-line-through': task.done }"
+              >
+                {{ task.title }}
+              </v-list-item-title>
+            </v-list-item-content>
+
+            <v-list-item-action>
+              <v-btn @click.stop="deleteTask(task.id)" icon>
+                <v-icon color="grey lighten-1">mdi-delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
+    </v-list>
+    <v-snackbar v-model="snackbar" :timeout="timeout">
+      {{ text }}
+      <template v-slot:action="{ attrs }">
+        <v-btn color="blue" text v-bind="attrs" @click="snackbar = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      tasks: [
+        /* { id: 1, title: "Sprint", done: false },
+        { id: 2, title: "Meditate", done: false },
+        { id: 3, title: "Sleep early", done: false }, */
+      ],
+      snackbar: false,
+      text: "POKUS, IWAN KAHIT PAMILYA PA",
+      timeout: 3000,
+      newTaskTitle: '',
+    };
+  },
+  methods: {
+    doneTask(id) {
+      let task = this.tasks.filter((task) => task.id === id)[0];
+      task.done = !task.done;
+      this.snackbar = true;
+    },
+    deleteTask(id) {
+      this.tasks = this.tasks.filter((task) => task.id !== id);
+    },
+    addTask(){
+      const newTask = {
+        id: Date.now(),
+        title: this.newTaskTitle,
+        done: false
+      }
+      this.tasks.push(newTask)
+      this.newTaskTitle = ''
+    }
+  },
+};
+</script>
